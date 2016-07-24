f08c50ff6530a43d36ce376a65f5eb5a289242cb
genshun9 <genshun@fringe81.com>
2016-07-24 22:57:14 +0900

    数字を分に変換するutilsを追加

---
+++ src/utils/dataUtils.js
@@ -0,0 +1,14 @@
+export function convertMinutesFromData(time) {
+  var hourPart = time.split(':')[0];
+  var minutePart = time.split(':')[1];
+  var minutes = Number(hourPart) * 60 + Number(minutePart);
+  return minutes;
+}
+
+export function convertDataFromMinutes(workTime, sumGenreTime) {
+  var restTime = workTime - sumGenreTime;
+  var hourPart = Math.floor(restTime / 60);
+  var minutePart = restTime - hourPart * 60;
+  var timeData = `${hourPart}:${minutePart}`;
+  return timeData;
+}
